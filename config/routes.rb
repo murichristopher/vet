3694f566c7eaf0ebc403e30a3dc7178b6b794c03
murichristopher <murichristopher@outlook.com>
2023-04-10 00:28:42 -0300

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,14 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  authenticated :user do
-    root to: 'dashboard#index', as: :authenticated_root
-    resources :customers, only: %i[index show new create edit update destroy]
-  end
+  # authenticated :user do
+  #   root to: 'dashboard#index', as: :authenticated_root
+  #   resources :customers, only: %i[index show new create edit update destroy]
+  # end
 
-  unauthenticated do
-    root to: 'landing_page#index'
-  end
+  # unauthenticated do
+  #   root to: 'landing_page#index'
+  # end
 
   # match '*path', to: redirect('/'), via: :all
 end
